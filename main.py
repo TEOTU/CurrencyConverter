bc35bdb711dfe3596a09dc0ba3e25f6b95350d10
Krasavchik <mhailtnko@gmail.com>
2024-02-07 12:36:17 +0100

    ui+functionality

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ def response_from_api():
     return rates_dict
 
 
+def ui():
+    st.markdown("<h1 style='background-color:cyan;text-align:center;color:white;'>Real-time currency converter</h1>",
+                unsafe_allow_html=True)
+    st.markdown("")
+    api_data = response_from_api()
+    if type(api_data) == dict:
+        cols = st.columns(2)
+        c1 = cols[0].selectbox("Convert from", api_data.keys())
+        c2 = cols[1].selectbox("Convert to", api_data.keys())
+        amount = cols[0].number_input("Amount", min_value=1)
+        convfactor = api_data[c2] / api_data[c1]
+        convertedamount = convfactor * amount
+        amount_after_conversion = cols[1].number_input('Converted amount', value=convertedamount)
+    else:
+        st.error("There is an issue connecting to the API.")
 
+
+if __name__ == '__main__':
+    response_from_api()
+    ui()
 
 
